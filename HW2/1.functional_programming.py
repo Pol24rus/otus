da77d18b4c10fa55a362902b73d6ac569844b32b
Pol24rus <pol_dv@mail.ru>
2024-01-12 18:50:31 +0700

    add HW_2 dir with files

---
+++ HW2/1.functional_programming.py
@@ -0,0 +1,14 @@
+def square(value: int) -> int:
+    return value ** 2
+
+
+def calc_sum(a: int, b: int) -> int:
+    return a + b
+
+
+def main():
+    print(square(calc_sum(2, 3)))
+
+
+if __name__ == "__main__":
+    main()
---
+++ HW2/5.function_scope.py
@@ -0,0 +1,10 @@
+x = 100
+
+
+def change_var(x=None):
+    x = 5
+    print(x)
+
+
+change_var(x)
+print(x)
