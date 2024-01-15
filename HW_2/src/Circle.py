63666c69c54c6c691e8a700c5e2aa1df7e1f717a
Pol24rus <poladko.dv@roslesinforg.ru>
2024-01-15 15:38:16 +0700

    add Circle.py, Triangle.py, Square.py

---
+++ HW_2/src/Circle.py
@@ -0,0 +1,23 @@
+from HW_2.src.Figure import Figure
+import math
+
+class Sircle(Figure):
+
+    def __init__(self, side_a):
+        super().__init__(name="Sircle")
+        if side_a <= 0:
+            raise ValueError("Диаметр круга должен быть больше нуля")
+        self.side_a = side_a
+
+
+    def get_area(self):
+        return math.pi * ((self.side_a / 2) ** 2)
+
+
+    def get_perimetr(self):
+        return self.side_a * math.pi
+
+
+scl = Sircle(5)
+print("Площадь круга = ", scl.get_area())
+print("Длина круга = ", scl.get_perimetr())
---
+++ HW_2/src/Square.py
@@ -0,0 +1,14 @@
+from HW_2.src.Rectangle import Rectangle
+
+
+class Square(Rectangle):
+
+    def __init__(self, side_a):
+        if side_a <= 0:
+            raise ValueError("Стороны квадрата должны быть больше нуля")
+        super().__init__(side_a, side_a)
+
+
+s = Square(6)
+print("Площадь квадрата=", s.get_area())
+print("Периметр квадрата=", s.get_perimetr())
