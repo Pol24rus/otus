ce1ca3821c6587a4425dcec9cb6ebf157ed7889b
Pol24rus <pol_dv@mail.ru>
2024-01-17 21:46:49 +0700

    промежуточный коммит

--- Tests/test_classes.py
+++ Tests/test_classes.py
@@ -1,6 +1,17 @@
+#import sys
 from src.Rectangle import Rectangle
+import pytest
+from datetime import datetime
 
 
-def test_positive():
+#print(sys.path)
+def test_positive_int():
     r = Rectangle(3, 4)
-    assert r.get_area() == 13
+    assert r.get_area() == 12
+
+
+#@pytest.mark.skipif(condition=datetime.now().month == 1, reason="Bug")
+@pytest.mark.smoke
+def test_positive_float():
+    r = Rectangle(3.5, 4.5)
+    assert r.get_area() == 15.75
