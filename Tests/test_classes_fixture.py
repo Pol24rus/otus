b433465462d628d37862cf4752a913889bae31dc
Pol24rus <pol_dv@mail.ru>
2024-01-18 19:37:08 +0700

    add conftest.py

---
+++ Tests/test_classes_fixture.py
@@ -0,0 +1,9 @@
+from src.Rectangle import Rectangle, Square
+import pytest
+# from datetime import datetime
+
+
+def test_positive(get_rectangle):
+    a, b, area = get_rectangle
+    r = Rectangle(a, b)
+    assert r.get_area() == area
