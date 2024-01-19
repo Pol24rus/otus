5bf1ff3da22f18caa5469ce4369fdc21e2c3874b
Pol24rus <pol_dv@mail.ru>
2024-01-20 00:39:56 +0700

    HW_3/2

--- back/test_classes_fixture.py
+++
@@ -1,9 +0,0 @@
-from src.Rectangle import Rectangle
-import pytest
-# from datetime import datetime
-
-
-def test_positive(get_rectangle):
-    a, b, area = get_rectangle
-    r = Rectangle(a, b)
-    assert r.get_area() == area
